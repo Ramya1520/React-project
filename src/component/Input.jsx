10d7a1a278146cd279990fc55c7e352163ff113d
Ramya1520 <ramya.krishnan1520@gmail.com>
2022-12-08 16:28:06 +0530

    change initial amount in every transaction

--- src/component/Input.jsx
+++ src/component/Input.jsx
@@ -4,10 +4,12 @@ import './StyleSheet.css'
 import EditList from "./Update"
 import InitialValue from './Initialvalue'
 function Crud() {
+    const [val,setVal]=useState(1000)
+
     const [list, setList] = useState([])
     const [listVal, setListVal] = useState({
         id:'',
-        amount: '',
+        amount:'',
         notes: '',
         type: ''
     })
@@ -35,28 +37,38 @@ function Crud() {
         setList(newList)
     }
     function updateData(updatedData) {
-        console.log(updatedData, "CRUD")
-        
+        console.log(updatedData, "CRUD")    
         const newList = list.map((li, index) => (update === index ?
             { ...li, id: updatedData.id,amount: updatedData.amount, notes: updatedData.notes, type: updatedData.type } : li))
         setList(newList)
         setUpdate(-1)
-     
-    }
+     }
     const handleUpdate = (event) => {
         event.preventDefault();
     }
-    function detail(props){
-        console.log("@@@@@@@@@",val)
-    }
-    const val=1000
     function credit (){
-       
-        const length = list.length;
-        console.log("length:"+length)
+        var length = list.length;
+        var index1=length-1
+        console.log("length:"+index1)
 
+       var current_amount=list[length-1].amount;
+        console.log("@@@@@@@@@",current_amount)
+        console.log("val",val);
+        var balance=parseInt(current_amount)+parseInt(val);
+        setVal(balance);
     }
-    
+    function debit(){
+        var length = list.length;
+        var index1=length-1
+        console.log("length:"+index1)
+        var current_amount=list[length-1].amount;
+        console.log("@@@@@@@@@",current_amount)
+        console.log("val",val);
+        var balance=parseInt(val)-parseInt(current_amount);
+        console.log(parseInt(current_amount))
+        console.log(parseInt(val))
+        setVal(balance);
+    }
     return (
         <div>
             <h1>Expense Tracker</h1>
@@ -73,13 +85,19 @@ function Crud() {
                     onChange={e => setListVal({ ...listVal, notes: e.target.value })} ref={notesRef} />
                 <label>Type :</label>
                 <input type="text" value={list.type}
-                    onChange={e => setListVal({ ...listVal, type: e.target.value })} ref={typeRef} />
-                
-                <button type="submit" onClick={valSubmit}>Add</button>
-                <button>j</button>
-                <button value={""} >credit</button>
-                <button type="debit" value={""}>debit</button>
+                    onChange={e => setListVal({ ...listVal, type: e.target.value })} ref={typeRef} />   
             </form>
+            {/* <select>
+                <option><button type="submit" onClick={valSubmit}>Add</button></option>
+                <option>Choose</option>
+                <option><button type="credit" value={""} onSelect={credit}>credit</button></option>
+                <option><button type="debit" value={""} onSelect={debit}>debit</button></option>
+            </select> */}
+            <div className="button">
+                <button type="submit" onClick={valSubmit}>Add</button>
+                <button type="credit" value={""} onClick={credit}>credit</button>
+                <button type="debit" value={""} onClick={debit}>debit</button>
+                </div>
             <div className="myTable">
                 <form onClick={handleUpdate}>
                     <table >
@@ -88,7 +106,6 @@ function Crud() {
                         <td>Notes</td>
                         <td>Type</td>
                         <td>Action</td>
-                       
                         {
                             list.map((element, index) => (
                                 update === index ? <EditList current={element} update={updateData} /> :
