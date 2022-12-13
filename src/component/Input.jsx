84958301e575b7b9e6704d03712d75da84250ac5
Ramya1520 <ramya.krishnan1520@gmail.com>
2022-12-13 11:22:21 +0530

    add balance amount in the table

--- src/component/Input.jsx
+++ src/component/Input.jsx
@@ -1,78 +1,77 @@
-import React, { useState, useRef } from "react"
+import React, { useState, useRef, useEffect } from "react"
 // import Submit from "./Submit"
 import './StyleSheet.css'
 import EditList from "./Update"
-import InitialValue from './Initialvalue'
 function Crud() {
-   
-    const [val,setVal]=useState(1000)
+    const [val, setVal] = useState(1000)
     const [list, setList] = useState([])
     const [listVal, setListVal] = useState({
-        id:'',
-        amount:'',
+        id: '',
+        amount: '',
         notes: '',
-        type: ''
+        type: '',
+        bal: ''
     })
-    
     const [update, setUpdate] = useState(-1)
-    const idRef=useRef()
+    const idRef = useRef()
     const amountRef = useRef()
     const notesRef = useRef()
     const typeRef = useRef()
+    console.log("list", list)
+    console.log("list val:", listVal)
+    useEffect(() => {
+        setListVal({ ...listVal, bal: val })
+    }, [val])
+    var a = []
+    a.push(listVal);
+    console.log("a===", a)
+    var length = a.length;
+    console.log("length:" + length)
+    var current_amount = a[length - 1].amount;
+    console.log("current value", current_amount)
+    console.log("val", val);
+    function credit() {
+        var balance = parseInt(current_amount) + parseInt(val);
+        setVal(balance);
+    }
+    function debit() {
+        var balance = parseInt(val) - parseInt(current_amount);
+        setVal(balance);
+    }
     const valSubmit = (event) => {
         event.preventDefault();
-        // if (listVal.amount && listVal.notes && listVal.type) { setList([...list, listVal]) }
-        if (listVal.id && listVal.amount && listVal.notes && listVal.type) { setList([...list, listVal]) }
-        idRef.current.value=''
+        if (listVal.id && listVal.amount && listVal.notes && listVal.type && listVal.bal) { setList([...list, listVal]) }
+        idRef.current.value = ''
         amountRef.current.value = ''
         notesRef.current.value = ''
         typeRef.current.value = ''
-        
-    }
-    var a=[]
-    a.push(listVal);
-    console.log("==>a:",a)
-    var length = a.length;
-    console.log("length:"+length)
-    var current_amount=a[length-1].amount;
-    console.log("current value",current_amount)
-    console.log("val",val);
-    function credit (){ 
-        var balance=parseInt(current_amount)+parseInt(val);
-        setVal(balance);
-    }
-    function debit(){
-        var balance=parseInt(val)-parseInt(current_amount);
-        setVal(balance);
     }
     function handleEdit(index) {
         setUpdate(index)
-    }   
+    }
     function handleDelete(id) {
         console.log("first")
         const newList = list.filter((li) => (li.id !== id))
         setList(newList)
     }
     function updateData(updatedData) {
-        console.log(updatedData, "CRUD")    
+        console.log(updatedData, "CRUD")
         const newList = list.map((li, index) => (update === index ?
-            { ...li, id: updatedData.id,amount: updatedData.amount, notes: updatedData.notes, type: updatedData.type } : li))
+            { ...li, id: updatedData.id, amount: updatedData.amount, notes: updatedData.notes, type: updatedData.type } : li))
         setList(newList)
         setUpdate(-1)
-     }
+    }
     const handleUpdate = (event) => {
         event.preventDefault();
     }
-   
-        // var type = (type==='credit') ? "credit" : "debit";
     return (
         <div>
             <h1>Expense Tracker</h1>
-            <p><InitialValue  initialAmount={val}/></p>            
+            <p>InitialAmount:{val}</p>
             <form className="forms" >
-            <label>Id :</label>
-            <input type="id" value={list.id}
-              onChange={e => setListVal({ ...listVal, id: e.target.value })} ref={idRef} />
+                <label>Id :</label>
+                <input type="id" value={list.id}
+                    onChange={e => setListVal({ ...listVal, id: e.target.value })} ref={idRef} />
                 <label>Amount :</label>
                 <input type="number" value={list.amount}
                     onChange={e => setListVal({ ...listVal, amount: e.target.value })} ref={amountRef} />
@@ -81,14 +80,14 @@ function Crud() {
                     onChange={e => setListVal({ ...listVal, notes: e.target.value })} ref={notesRef} />
                 <label>Type :</label>
                 <input type="text" value={list.type}
-                    onChange={e => setListVal({ ...listVal, type: e.target.value })} ref={typeRef} />   
+                    onChange={e => setListVal({ ...listVal, type: e.target.value })} ref={typeRef} />
             </form>
             <div className="button">
-            <button type="credit" value={""} onClick={credit}>credit</button>
+                <button type="credit" value={""} onClick={credit}>credit</button>
                 <button type="debit" value={""} onClick={debit}>debit</button>
                 <button type="submit" onClick={valSubmit}>Add</button>
-                
-                </div>
+
+            </div>
             <div className="myTable">
                 <form onClick={handleUpdate}>
                     <table >
@@ -96,6 +95,7 @@ function Crud() {
                         <td>Amount</td>
                         <td>Notes</td>
                         <td>Type</td>
+                        <td>bal</td>
                         <td>Action</td>
                         {
                             list.map((element, index) => (
@@ -105,6 +105,7 @@ function Crud() {
                                         <td>{element.amount}</td>
                                         <td>{element.notes}</td>
                                         <td>{element.type}</td>
+                                        <td>{element.bal}</td>
                                         <td>
                                             <button className="edit" onClick={() => handleEdit(index)}>Edit</button>
                                             <button className="delete" onClick={() => handleDelete(element.amount)}>Delete</button>
@@ -117,6 +118,6 @@ function Crud() {
             </div>
         </div>
     )
-  
+
 }
 export default Crud
