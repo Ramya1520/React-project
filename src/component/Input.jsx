4dff01a851c3fe29ac56b23fa25dc8d5e6b7221b
Ramya1520 <ramya.krishnan1520@gmail.com>
2022-12-14 01:16:35 +0530

    add type of the transaction

--- src/component/Input.jsx
+++ src/component/Input.jsx
@@ -2,10 +2,10 @@ import React, { useState, useRef, useEffect } from "react"
 // import Submit from "./Submit"
 import './StyleSheet.css'
 import EditList from "./Update"
-var no=1
+var no = 1
 function Crud() {
-   
-    console.log("no:",no)
+    console.log("no:", no)
+    const [type, setType] = useState()
     const [val, setVal] = useState(1000)
     const [list, setList] = useState([])
     const [listVal, setListVal] = useState({
@@ -15,20 +15,10 @@ function Crud() {
         type: '',
         bal: ''
     })
+
     const [update, setUpdate] = useState(-1)
-   
     const amountRef = useRef()
     const notesRef = useRef()
-    const typeRef = useRef()
-    console.log("list", list)
-    console.log("list val:", listVal)
-    useEffect(() => {
-        setListVal({ ...listVal, bal: val })
-     
-    }, [val])
-    useEffect(()=>{
-        setListVal({...listVal, id:no})
-    },[no])
 
     var a = []
     a.push(listVal);
@@ -38,30 +28,46 @@ function Crud() {
     var current_amount = a[length - 1].amount;
     console.log("current value", current_amount)
     console.log("val", val);
+
     function credit() {
         var balance = parseInt(current_amount) + parseInt(val);
         setVal(balance);
-    
+        var types = "credit"
+        setType(types);
+        setListVal({ ...listVal, type: types })
     }
+
     function debit() {
         var balance = parseInt(val) - parseInt(current_amount);
         setVal(balance);
-      
+        var types = "debit"
+        setType(types);
+        console.log("type===============>", type)
+        setListVal({ ...listVal, type: types })
     }
-    const valSubmit = (event) => {
-        no=no+1
-        event.preventDefault();
-        if (listVal.id || listVal.amount && listVal.notes && listVal.type && listVal.bal) { setList([...list, listVal]) }
 
+    const valSubmit = (event) => {
+        no = no + 1
+        event.preventDefault();
+        if (listVal.id || listVal.amount || listVal.notes || listVal.type || listVal.bal) { setList([...list, listVal]) }
         amountRef.current.value = ''
         notesRef.current.value = ''
-        typeRef.current.value = '' 
+        console.log("list=================================>:", list)
     }
+
+    useEffect(() => {
+        setListVal({ ...listVal, bal: val })
+    }, [val])
+    useEffect(() => {
+        setListVal({ ...listVal, id: no })
+    }, [no])
+    
     function handleDelete(id) {
-        console.log("first",id)
+        console.log("first", id)
         const newList = list.filter((li) => (li.id !== id))
         setList(newList)
     }
+
     function updateData(updatedData) {
         console.log(updatedData, "CRUD")
         const newList = list.map((li, index) => (update === index ?
@@ -69,34 +75,30 @@ function Crud() {
         setList(newList)
         setUpdate(-1)
     }
+
     const handleUpdate = (event) => {
         event.preventDefault();
     }
+
     return (
-        
+
         <div>
             <h1>Expense Tracker</h1>
             <p>InitialAmount:{val}</p>
             <form className="forms" >
-                {/* <label>Id :</label>
-                <input type="id" value={list.id}
-                    onChange={e => setListVal({ ...listVal, id: e.target.value })} ref={idRef} /> */}
                 <label>Amount :</label>
                 <input type="number" value={list.amount}
                     onChange={e => setListVal({ ...listVal, amount: e.target.value })} ref={amountRef} />
                 <label>Notes :</label>
                 <input type="text" value={list.notes}
-                    onChange={e => setListVal({ ...listVal, notes: e.target.value })} ref={notesRef} />
-                <label>Type :</label>
-                <input type="text" value={list.type}
-                    onChange={e => setListVal({ ...listVal, type: e.target.value })} ref={typeRef} />
+                    onChange={e => setListVal({ ...listVal, notes: e.target.value })} ref={notesRef} />      
             </form>
             <div className="button">
                 <button type="credit" value={""} onClick={credit}>credit</button>
                 <button type="debit" value={""} onClick={debit}>debit</button>
                 <button type="submit" onClick={valSubmit}>Add</button>
-
             </div>
+
             <div className="myTable">
                 <form onClick={handleUpdate}>
                     <table >
